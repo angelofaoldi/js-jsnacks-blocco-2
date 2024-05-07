e2ca0aaaa975d6c1339b357432c325b40d0a7c75
angelofaoldi <faoldiangelo@gmail.com>
2024-05-07 10:27:32 +0200

    [fix] js

--- snack4/README.md
+++ snack4/README.md
@@ -1,5 +1,8 @@
 Crea due array che hanno un numero di elementi diversi.
+
+
 Aggiungi elementi casuali all’array che ha meno elementi,
+
 fino a quando ne avrà tanti quanti l’altro.
 
 ________________________________________
--- snack4/snack4.js
+++ snack4/snack4.js
@@ -2,5 +2,35 @@
 
 // CREDO DUE ARRAY CON ELEMENTI DIVERSI
 
-const ARRAY1 = [ariete, toro, gemelli, cancro];
-const ARRA2 = [leone, vergine, bilancia, scorpione, sagittario];
+const UNO = ["ariete", "toro", "gemelli", "cancro"];
+console.log(UNO);
+
+const DUE = [
+    "leone", "vergine", "bilancia", "scorpione", "sagittario"];
+console.log(DUE);
+
+/*  Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro. */
+
+// CICLO FOR
+
+for (let i = UNO.lenght; i < DUE.lenght; i++)
+
+    // INSERISCO ELEMENTO RANDOM PER EQUIPARARE GLI ARRAY
+
+    {
+        
+        const randomElement = DUE[Math.floor(Math.random() 
+                                            * DUE.length)];
+
+        UNO.push(randomElement);
+    
+        // 
+    
+    }
+
+console.log("Array 1: ", UNO);
+console.log("Array 2: ", DUE);
+
+
+ 
